a8742c126dbc410c32757fd8f3f17ec63e612055
SirakMario <sirak.nafu@gmail.com>
2024-07-27 14:41:53 +0000

    Data Preprocessing and Training the model with augmented data

---
+++ Data_Preprocessing.py
@@ -0,0 +1,223 @@
+"""
+
+The landcover dataset from here: https://landcover.ai/
+
+
+Tasks 
+1. Read large images and corresponding masks, divide them into smaller patches.
+And write the patches as images to the local drive.  
+
+2. Save only images and masks where masks have some decent amount of labels other than 0. 
+Using blank images with label=0 is a waste of time and may bias the model towards 
+unlabeled pixels. 
+
+3. Divide the sorted dataset from above into train, test and validation datasets. 
+
+4. You have to manually create and move some folders and rename appropriately and use 
+ImageDataGenerator from keras. 
+
+"""
+
+import os
+import cv2
+import numpy as np
+import glob
+
+import numpy as np
+from matplotlib import pyplot as plt
+from patchify import patchify
+import tifffile as tiff
+from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
+import segmentation_models as sm
+from tensorflow.keras.metrics import MeanIoU
+import random
+
+#Understanding of the dataset
+temp_img = cv2.imread("data/images/M-34-51-C-d-4-1.tif") #3 channels / spectral bands
+plt.imshow(temp_img[:,:,2]) #View each channel...
+temp_mask = cv2.imread("data/masks/M-34-51-C-d-4-1.tif") #3 channels but all same. 
+labels, count = np.unique(temp_mask[:,:,0], return_counts=True) #Check for each channel. All chanels are identical
+print("Labels are: ", labels, " and the counts are: ", count)
+
+#Crop each large image into patches of 256x256. Save them into a directory 
+root_directory = 'data/'
+
+patch_size = 256
+
+#Read images from repsective 'images' subdirectory
+#As all images are of different size we have 2 options, either resize or crop
+#But, some images are too large and some small. Resizing will change the size of real objects.
+#Therefore, we will crop them to a nearest size divisible by 256 and then 
+#divide all images into patches of 256x256x3. 
+img_dir=root_directory+"images/"
+for path, subdirs, files in os.walk(img_dir):
+    dirname = path.split(os.path.sep)[-1]
+    images = os.listdir(path)  #List of all image names in this subdirectory
+    for i, image_name in enumerate(images):  
+        if image_name.endswith(".tif"):
+            image = cv2.imread(path+"/"+image_name, 1)  #Read each image as BGR
+            SIZE_X = (image.shape[1]//patch_size)*patch_size #Nearest size divisible by our patch size
+            SIZE_Y = (image.shape[0]//patch_size)*patch_size #Nearest size divisible by our patch size
+            image = Image.fromarray(image)
+            image = image.crop((0 ,0, SIZE_X, SIZE_Y))  #Crop from top left corner
+            image = np.array(image)             
+   
+            #Extract patches from each image
+            print("Now patchifying image:", path+"/"+image_name)
+            patches_img = patchify(image, (256, 256, 3), step=256)
+    
+            for i in range(patches_img.shape[0]):
+                for j in range(patches_img.shape[1]):
+                    
+                    single_patch_img = patches_img[i,j,:,:]
+                    single_patch_img = single_patch_img[0] #Drop the extra unecessary dimension that patchify adds.                               
+                    
+                    cv2.imwrite(root_directory+"256_patches/images/"+
+                               image_name+"patch_"+str(i)+str(j)+".tif", single_patch_img)
+            
+  
+ #Do the same as above for masks
+mask_dir=root_directory+"masks/"
+for path, subdirs, files in os.walk(mask_dir):
+    dirname = path.split(os.path.sep)[-1]
+
+    masks = os.listdir(path)  #List of all image names in this subdirectory
+    for i, mask_name in enumerate(masks):  
+        if mask_name.endswith(".tif"):           
+            mask = cv2.imread(path+"/"+mask_name, 0)  #Read each image as Grey
+            SIZE_X = (mask.shape[1]//patch_size)*patch_size #Nearest size divisible by our patch size
+            SIZE_Y = (mask.shape[0]//patch_size)*patch_size #Nearest size divisible by our patch size
+            mask = Image.fromarray(mask)
+            mask = mask.crop((0 ,0, SIZE_X, SIZE_Y))  #Crop from top left corner
+            mask = np.array(mask)             
+   
+            #Extract patches from each image
+            print("Now patchifying mask:", path+"/"+mask_name)
+            patches_mask = patchify(mask, (256, 256), step=256)    
+            for i in range(patches_mask.shape[0]):
+                for j in range(patches_mask.shape[1]):
+                    
+                    single_patch_mask = patches_mask[i,j,:,:]
+                    cv2.imwrite(root_directory+"256_patches/masks/"+
+                               mask_name+"patch_"+str(i)+str(j)+".tif", single_patch_mask)
+
+
+
+train_img_dir = "data/256_patches/images/"
+train_mask_dir = "data/256_patches/masks/"
+
+img_list = os.listdir(train_img_dir)
+msk_list = os.listdir(train_mask_dir)
+
+num_images = len(os.listdir(train_img_dir))
+
+
+img_num = random.randint(0, num_images-1)
+
+img_for_plot = cv2.imread(train_img_dir+img_list[img_num], 1)
+img_for_plot = cv2.cvtColor(img_for_plot, cv2.COLOR_BGR2RGB)
+
+mask_for_plot =cv2.imread(train_mask_dir+msk_list[img_num], 0)
+
+plt.figure(figsize=(12, 8))
+plt.subplot(121)
+plt.imshow(img_for_plot)
+plt.title('Image')
+plt.subplot(122)
+plt.imshow(mask_for_plot, cmap='gray')
+plt.title('Mask')
+plt.show()
+
+###########################################################################
+# real information = if mask has decent amount of labels other than 0. 
+
+useless=0
+for img in range(len(img_list)): 
+    img_name=img_list[img]
+    mask_name = msk_list[img]
+    print("Now preparing image and masks number: ", img)
+      
+    temp_image=cv2.imread(train_img_dir+img_list[img], 1)
+   
+    temp_mask=cv2.imread(train_mask_dir+msk_list[img], 0)
+    
+    val, counts = np.unique(temp_mask, return_counts=True)
+    
+    if (1 - (counts[0]/counts.sum())) > 0.05:  #At least 5% useful area with labels that are not 0
+        cv2.imwrite('data/256_patches/images_with_useful_info/images/'+img_name, temp_image)
+        cv2.imwrite('data/256_patches/images_with_useful_info/masks/'+mask_name, temp_mask)
+        
+    else:
+        useless +=1
+
+print("Total useful images are: ", len(img_list)-useless)
+print("Total useless images are: ", useless)
+###############################################################
+#Split the data into training, validation and testing. 
+
+"""
+Code for splitting folder into train, test, and val.
+Once the new folders are created rename them and arrange in the format below to be used
+for semantic segmentation using data generators. 
+"""
+import splitfolders
+
+input_folder = 'data/256_patches/images_with_useful_info/'
+output_folder = 'data/data_for_training_testing_val/'
+splitfolders.ratio(input_folder, output=output_folder, seed=42, ratio=(.80, 0.10,0.10), group_prefix=None)
+########################################
+
+#Manually move folders around to bring them to the following structure.
+"""
+Your current directory structure:
+Data/
+    train/
+        images/
+            img1, img2, ...
+        masks/
+            msk1, msk2, ....
+            
+    test/
+        images/
+            img1, img2, ...
+        masks/
+            msk1, msk2, ....
+            
+    val/
+        images/
+            img1, img2, ...
+        masks/
+            msk1, msk2, ....
+        
+Copy the folders around to the following structure... 
+
+
+Data/
+    train_images/
+                train/
+                    img1, img2, img3, ......
+    
+    train_masks/
+                train/
+                    msk1, msk, msk3, ......
+    
+    test_images/
+                test/
+                    img1, img2, img3, ......                
+
+    test_masks/
+                test/
+                    msk1, msk, msk3, ......
+    
+    val_images/
+                val/
+                    img1, img2, img3, ......                
+
+    val_masks/
+                val/
+                    msk1, msk, msk3, ......
+      
+                    
+"""
---
+++ training_landcover_with_augmentation.py
@@ -0,0 +1,232 @@
+"""
+Dataset from : https://landcover.ai/
+
+labels:
+    0: Unlabeled background 
+    1: Buildings
+    2: Woodlands
+    3: Water
+    4: Road
+
+"""
+
+import os
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import segmentation_models as sm
+from tensorflow.keras.metrics import MeanIoU
+import random
+
+################################################################
+#Understanding the data by looking at a few random images and masks 
+
+train_img_dir = "data/data_for_keras_aug/train_images/train/"
+train_mask_dir = "data/data_for_keras_aug/train_masks/train/"
+
+img_list = os.listdir(train_img_dir)
+msk_list = os.listdir(train_mask_dir)
+
+num_images = len(os.listdir(train_img_dir))
+
+
+img_num = random.randint(0, num_images-1)
+
+img_for_plot = cv2.imread(train_img_dir+img_list[img_num], 1)
+img_for_plot = cv2.cvtColor(img_for_plot, cv2.COLOR_BGR2RGB)
+
+mask_for_plot =cv2.imread(train_mask_dir+msk_list[img_num], 0)
+
+plt.figure(figsize=(12, 8))
+plt.subplot(121)
+plt.imshow(img_for_plot)
+plt.title('Image')
+plt.subplot(122)
+plt.imshow(mask_for_plot, cmap='gray')
+plt.title('Mask')
+plt.show()
+
+################################################################
+
+# Define Generator for images and masks so we can read them directly from the drive. 
+
+seed=24
+batch_size= 8
+n_classes=5
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+from keras.utils import to_categorical
+
+#Use this to preprocess input for transfer learning
+BACKBONE = 'resnet34'
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+#Define a function to perform additional preprocessing after datagen.
+def preprocess_data(img, mask, num_class):
+    #Scale images
+    img = scaler.fit_transform(img.reshape(-1, img.shape[-1])).reshape(img.shape)
+    img = preprocess_input(img)  #Preprocess based on the pretrained backbone resnet34
+    #Convert mask to one-hot
+    mask = to_categorical(mask, num_class)
+      
+    return (img,mask)
+
+#Define the generator.
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+def trainGenerator(train_img_path, train_mask_path, num_class):
+    
+    img_data_gen_args = dict(horizontal_flip=True,
+                      vertical_flip=True,
+                      fill_mode='reflect')
+    
+    image_datagen = ImageDataGenerator(**img_data_gen_args)
+    mask_datagen = ImageDataGenerator(**img_data_gen_args)
+    
+    image_generator = image_datagen.flow_from_directory(
+        train_img_path,
+        class_mode = None,
+        batch_size = batch_size,
+        seed = seed)
+    
+    mask_generator = mask_datagen.flow_from_directory(
+        train_mask_path,
+        class_mode = None,
+        color_mode = 'grayscale',
+        batch_size = batch_size,
+        seed = seed)
+    
+    train_generator = zip(image_generator, mask_generator)
+    
+    for (img, mask) in train_generator:
+        img, mask = preprocess_data(img, mask, num_class)
+        yield (img, mask)
+
+
+train_img_path = "data/data_for_keras_aug/train_images/"
+train_mask_path = "data/data_for_keras_aug/train_masks/"
+train_img_gen = trainGenerator(train_img_path, train_mask_path, num_class=4)
+
+val_img_path = "data/data_for_keras_aug/val_images/"
+val_mask_path = "data/data_for_keras_aug/val_masks/"
+val_img_gen = trainGenerator(val_img_path, val_mask_path, num_class=4)
+
+#Check the generator is working and the images and masks are indeed lined up. 
+x, y = train_img_gen.__next__()
+
+for i in range(0,3):
+    image = x[i]
+    mask = np.argmax(y[i], axis=2)
+    plt.subplot(1,2,1)
+    plt.imshow(image)
+    plt.subplot(1,2,2)
+    plt.imshow(mask, cmap='gray')
+    plt.show()
+
+x_val, y_val = val_img_gen.__next__()
+
+for i in range(0,3):
+    image = x_val[i]
+    mask = np.argmax(y_val[i], axis=2)
+    plt.subplot(1,2,1)
+    plt.imshow(image)
+    plt.subplot(1,2,2)
+    plt.imshow(mask, cmap='gray')
+    plt.show()
+
+###########################################################################
+#Define the model metrcis and load model. 
+
+num_train_imgs = len(os.listdir('data/data_for_keras_aug/train_images/train/'))
+num_val_images = len(os.listdir('data/data_for_keras_aug/val_images/val/'))
+steps_per_epoch = num_train_imgs//batch_size
+val_steps_per_epoch = num_val_images//batch_size
+
+
+IMG_HEIGHT = x.shape[1]
+IMG_WIDTH  = x.shape[2]
+IMG_CHANNELS = x.shape[3]
+
+n_classes=4
+
+#############################################################################
+#Define the model
+model = sm.Unet(BACKBONE, encoder_weights='imagenet', 
+                input_shape=(IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS),
+                classes=n_classes, activation='softmax')
+model.compile('Adam', loss=sm.losses.categorical_focal_jaccard_loss, metrics=[sm.metrics.iou_score])
+
+print(model.summary())
+print(model.input_shape)
+
+#Train the model. 
+history=model.fit(train_img_gen,
+          steps_per_epoch=steps_per_epoch,
+          epochs=25,
+          verbose=1,
+          validation_data=val_img_gen,
+          validation_steps=val_steps_per_epoch)
+
+model.save('landcover_agumented_u-net.hdf5')
+
+##################################################################
+#plot the training and validation IoU and loss at each epoch
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+acc = history.history['iou_score']
+val_acc = history.history['val_iou_score']
+
+plt.plot(epochs, acc, 'y', label='Training IoU')
+plt.plot(epochs, val_acc, 'r', label='Validation IoU')
+plt.title('Training and validation IoU')
+plt.xlabel('Epochs')
+plt.ylabel('IoU')
+plt.legend()
+plt.show()
+
+#####################################################
+
+from keras.models import load_model
+
+model = load_model("landcover_agumented_u-net.hdf5", compile=False)
+
+
+#Test generator using validation data.
+
+test_image_batch, test_mask_batch = val_img_gen.__next__()
+
+#Convert categorical to integer for visualization and IoU calculation
+test_mask_batch_argmax = np.argmax(test_mask_batch, axis=3) 
+test_pred_batch = model.predict(test_image_batch)
+test_pred_batch_argmax = np.argmax(test_pred_batch, axis=3)
+
+n_classes = 5
+IOU_keras = MeanIoU(num_classes=n_classes)  
+IOU_keras.update_state(test_pred_batch_argmax, test_mask_batch_argmax)
+print("Mean IoU =", IOU_keras.result().numpy())
+
+#######################################################
+#View a few images, masks and corresponding predictions. 
+img_num = random.randint(0, test_image_batch.shape[0]-1)
+
+plt.figure(figsize=(12, 8))
+plt.subplot(231)
+plt.title('Testing Image')
+plt.imshow(test_image_batch[img_num])
+plt.subplot(232)
+plt.title('Testing Label')
+plt.imshow(test_mask_batch_argmax[img_num])
+plt.subplot(233)
+plt.title('Prediction on test image')
+plt.imshow(test_pred_batch_argmax[img_num])
+plt.show()
+
